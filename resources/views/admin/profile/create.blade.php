2ae24d6f3dd7f5da38c22ff8a4dc7c67e926b481
erica <ec2-user@ip-172-31-23-84.us-east-2.compute.internal>
2019-09-14 05:36:49 +0000

    Laravel12-1

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.profile')
-@section('title','MyProfile')
+@section('title','Myプロフィール')
 @section('content')
 <div class="container">
   <div class="row">
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.profile')
-@section('title','MyProfile')
+@section('title','edite.blade.phpファイル')
 @section('content')
 <div class="container">
   <div class="row">
